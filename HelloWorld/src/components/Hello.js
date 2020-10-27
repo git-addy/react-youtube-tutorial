4a751758789facdd978e1849bd311c4f1e1225b8
git-addy <a.furtado@gmx.de>
2020-10-27 19:02:02 +0100

    Hello.js added from the JSX lecture to understand how html is working inside React framework

---
+++ HelloWorld/src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hello = () => {
+    return (
+        <div>
+            <h1>Hello Addy From over the Hood</h1>
+        </div>
+        )
+    
+}
+
+export default Hello
