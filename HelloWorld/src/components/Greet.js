eff0b7bd4a27bcaa0e53134c7eadae0ca6625edb
git-addy <a.furtado@gmx.de>
2020-10-27 10:52:31 +0100

    this is the first commit to the github rep, it includes two greet functions. One with functional component and one with class component

---
+++ HelloWorld/src/components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ const Greet = () => <h1>Hello Addy!</h1>
+
+// for connecting this to the rest of of the app
+// we need to export it from here
+// and import it into App.js
+export default Greet
